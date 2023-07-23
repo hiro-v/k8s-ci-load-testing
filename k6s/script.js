59a8acb1b268f8583f452fd14bcccc35a4f5ecd5
vuonghoainam <vuonghoainam.work@gmail.com>
2023-07-24 00:06:06 +0700

    chore(k6s): Change to 1 vus and export to csv

--- k6s/script.js
+++ k6s/script.js
@@ -2,8 +2,8 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export let options = {
-  vus: 10, // 10 virtual users
-  duration: "30s", // for 30 seconds
+  vus: 1, // 1 virtual users
+  duration: "10s", // for 30 seconds
 };
 
 export default function () {
